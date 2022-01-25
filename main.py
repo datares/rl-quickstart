6107b432f3351a88f8dd3911be6691b5182e7b2d
Colin Curtis <colinpcurtis826@ucla.edu>
2022-01-24 23:24:48 -0800

    add signal handler to save model

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from gym.wrappers import Monitor
 from stable_baselines3 import PPO
 from stable_baselines3.common.logger import configure
 from datetime import datetime
+import signal
 
 now = datetime.now()
 dt_string = now.strftime("%y-%m-%d %H:%M:%S")
@@ -12,7 +13,6 @@ def wrap_env(env):
     return env
 
 log_path = f"logs/{dt_string}"
-# # set up logger
 logger = configure(log_path, ["stdout", "tensorboard"])
 
 env = gym.make('Breakout-v0') # or Breakout-v0 for Atari
@@ -26,10 +26,18 @@ obs = env.reset()
 for i in range(100000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = env.step(action)
-    # env.render()
+    env.render()
     if done:
       obs = env.reset()
 
 env.close()
 
-model.save("sac_pendulum")
+
+def handle_signal(sig, frame):
+    print("Exiting gracefully, saving model")
+    model.save(f"{dt_string}.pkl")
+
+
+signal.signal(signal.SIGINT, handle_signal)
+
+model.save(f"{dt_string}.pkl")
