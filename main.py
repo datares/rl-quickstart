b401d6429c08e8bb5d10137ea4c074999fe3f191
Colin Curtis <colinpcurtis826@ucla.edu>
2022-01-24 21:35:31 -0800

    add model saving

--- main.py
+++ main.py
@@ -15,19 +15,21 @@ log_path = f"logs/{dt_string}"
 # # set up logger
 logger = configure(log_path, ["stdout", "tensorboard"])
 
-env = gym.make('CartPole-v1') # or Breakout-v0 for Atari
+env = gym.make('Breakout-v0') # or Breakout-v0 for Atari
 env = wrap_env(env)
 
 model = PPO('MlpPolicy', env, verbose=1)
 model.set_logger(logger)
-model.learn(total_timesteps=100000)
+model.learn(total_timesteps=10000000)
 
 obs = env.reset()
-for i in range(10000):
+for i in range(100000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = env.step(action)
-    env.render()
+    # env.render()
     if done:
       obs = env.reset()
 
 env.close()
+
+model.save("sac_pendulum")
