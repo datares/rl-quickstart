12e0ea1b8f20fc685a0011f79e734f3b27f97615
Colin Curtis <colinpcurtis826@ucla.edu>
2022-02-28 19:22:04 -0800

    clean up

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from gym.wrappers import Monitor
 from stable_baselines3 import PPO
 from stable_baselines3.common.logger import configure
 from datetime import datetime
-import signal
+
+ENVIRONMENT = "FetchPickAndPlace-v1"
 
 now = datetime.now()
 dt_string = now.strftime("%y-%m-%d %H:%M:%S")
 
 def wrap_env(env):
-    env = Monitor(env, 'video/', force=True)
+    env = Monitor(env, f'video/{ENVIRONMENT}-{dt_string}', force=True)
     return env
 
-log_path = f"logs/{dt_string}"
+log_path = f"logs/{ENVIRONMENT}-{dt_string}"
 logger = configure(log_path, ["stdout", "tensorboard"])
 
-env = gym.make('Breakout-v0') # or Breakout-v0 for Atari
+env = gym.make(ENVIRONMENT)
 env = wrap_env(env)
 
 model = PPO('MlpPolicy', env, verbose=1)
@@ -23,21 +24,16 @@ model.set_logger(logger)
 model.learn(total_timesteps=10000000)
 
 obs = env.reset()
+max_reward = 0
 for i in range(100000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = env.step(action)
     env.render()
+    if reward > max_reward:
+        model.save(f"models/{ENVIRONMENT}-{dt_string}-reward={reward}.pkl")
+        print(f"max reward = {reward}")
+        max_reward = reward
     if done:
       obs = env.reset()
 
 env.close()
-
-
-def handle_signal(sig, frame):
-    print("Exiting gracefully, saving model")
-    model.save(f"{dt_string}.pkl")
-
-
-signal.signal(signal.SIGINT, handle_signal)
-
-model.save(f"{dt_string}.pkl")
